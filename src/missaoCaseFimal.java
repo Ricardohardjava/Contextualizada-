d81c024008908f1e67931f8365f4a4e928e215d9
Ricardohardjava <ricardoalbuquerquecorretor@gmail.com>
2022-01-08 12:10:25 -0300

    Primeito commit

---
+++ src/missaoCaseFimal.java
@@ -0,0 +1,21 @@
+public class missaoCaseFimal {
+
+    public static void main(String[] args) {
+
+        int s=0;
+
+        int z=19-2+5;
+
+        int c=z % 6;
+
+        for (int i= 1; i<c+7;i=i+4) {
+            s=s+i +1;
+        }
+
+        System.out.println(s);
+
+
+
+    }
+
+}
